42b3f8011cc5f5e96a9fbe77655c2c5434409218
AndreySemeynikovIF <andrey.semeynikov@ifortex.com>
2025-02-07 12:01:11 +0300

    add search by user last name

--- user-management-api/src/main/java/com/ifortex/internship/usermanagementapi/dto/request/UserSearchRequest.java
+++ user-management-api/src/main/java/com/ifortex/internship/usermanagementapi/dto/request/UserSearchRequest.java
@@ -1,16 +1,17 @@
 package com.ifortex.internship.usermanagementapi.dto.request;
 
-import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 public class UserSearchRequest {
-    private String name;
+    private String firstName;
+    private String lastName;
     private String phone;
     private String email;
     private List<String> roles;
     private String status;
-    private Boolean deleted;
 }
--- user-management/src/main/java/com/ifortex/internship/usermanagement/repository/UserRepository.java
+++ user-management/src/main/java/com/ifortex/internship/usermanagement/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.ifortex.internship.usermanagement.repository;
 
 import com.ifortex.internship.usermanagement.model.User;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -10,20 +8,30 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
   Optional<User> findByUserId(String userId);
 
   List<User> findByUserIdIn(List<String> userIds);
 
-  default Page<User> findByFilters(String name, String phone, Pageable pageable) {
+  default Page<User> findByFilters(String firstName, String lastName, String phone, Pageable pageable) {
     Specification<User> spec = Specification.where(null);
 
-    if (name != null && !name.isEmpty()) {
+    if (firstName != null && !firstName.isEmpty()) {
       spec =
           spec.and(
               (root, query, cb) ->
-                  cb.like(cb.lower(root.get("firstName")), "%" + name.toLowerCase() + "%"));
+                  cb.like(cb.lower(root.get("firstName")), "%" + firstName.toLowerCase() + "%"));
+    }
+
+    if (lastName != null && !lastName.isEmpty()) {
+      spec =
+              spec.and(
+                      (root, query, cb) ->
+                              cb.like(cb.lower(root.get("lastName")), "%" + lastName.toLowerCase() + "%"));
     }
 
     if (phone != null && !phone.isEmpty()) {
